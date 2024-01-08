d9a3cd09c30791f0abb81bd587f40e93b6dbc3ab
Edward Kirakosyan <kirakosyan3010@gmail.com>
2024-01-08 20:10:06 +0400

    c

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { TodoGroupComponent } from './components/todo-group/todo-group.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, TodoGroupComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'train-test-ang';
--- src/app/components/todo-group/todo-group.component.ts
+++ src/app/components/todo-group/todo-group.component.ts
@@ -1,12 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { TodoItemComponent } from '../todo-item/todo-item.component';
 
 @Component({
   selector: 'app-todo-group',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, TodoItemComponent],
   templateUrl: './todo-group.component.html',
-  styleUrl: './todo-group.component.scss'
+  styleUrl: './todo-group.component.scss',
 })
-export class TodoGroupComponent {
-
-}
+export class TodoGroupComponent {}
